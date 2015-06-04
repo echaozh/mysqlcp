f261edf75f757d56cd0716010b11b0f77b4f0401
Zhang Yichao <echaozh@gmail.com>
2015-06-04 16:11:53 +0800

    Update sql_stmt.hpp

--- sql_stmt.hpp
+++ sql_stmt.hpp
@@ -1,7 +1,6 @@
 /// sql_stmt.hpp -- sql statement decls
 
-/// Copyright (c) 2011 Vobile. All rights reserved.
-/// Author: Zhang Yichao <zhang_yichao@vobile.cn>
+/// Author: Zhang Yichao <echaozh@gmail.com>
 /// Created: 2011-08-03
 ///
 
