d616d2eec504868b5771996ad81a50669c410bcf
Zhang Yichao <echaozh@gmail.com>
2015-06-04 16:11:32 +0800

    Update sql_res.hpp

--- sql_res.hpp
+++ sql_res.hpp
@@ -1,7 +1,6 @@
 /// sql_res.hpp -- sql results decls
 
-/// Copyright (c) 2011 Vobile. All rights reserved.
-/// Author: Zhang Yichao <zhang_yichao@vobile.cn>
+/// Author: Zhang Yichao <echaozh@gmail.com>
 /// Created: 2011-08-03
 ///
 
