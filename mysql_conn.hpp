da8ed9f72fa9708fd4fc1815bbf22993cde87584
Zhang Yichao <echaozh@gmail.com>
2015-06-04 16:10:59 +0800

    Update mysql_conn.hpp

--- mysql_conn.hpp
+++ mysql_conn.hpp
@@ -1,7 +1,6 @@
 /// mysql_conn.hpp -- mysql connection wrapper decls
 
-/// Copyright (c) 2011 Vobile. All rights reserved.
-/// Author: Zhang Yichao <zhang_yichao@vobile.cn>
+/// Author: Zhang Yichao <echaozh@gmail.com>
 /// Created: 2011-08-03
 ///
 
